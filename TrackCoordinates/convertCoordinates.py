8600336a0047478141aa78fb36dcf453bbfd6dc0
Jacob Salomonsen <jiekebo@gmail.com>
2013-09-06 18:53:17 +0200

    Updated conversion script to produce javascript 'friendly' data.

--- TrackCoordinates/convertCoordinates.py
+++ TrackCoordinates/convertCoordinates.py
@@ -1,14 +1,15 @@
 import sys
 
 def main():
-	outstring = ""
+	outstring = "["
 	with open(sys.argv[1]) as f:
 		content = f.readlines()
 		for coordinates in content:
 			x, y = coordinates.split(',')
 			convertedx = float(x) * 10000 / 1440
 			convertedy = float(y) * 10000 / 743
-			outstring += str(int(round(convertedx))) + ", " + str(int(round(convertedy))) + "\n"
+			outstring += "{x:" + str(int(round(convertedx))) + ", y:" + str(int(round(convertedy))) + "},"
+	outstring = outstring[:-1] + "]"
 	outf = file(sys.argv[1]+'conv', 'w')
 	outf.write(outstring)
 
