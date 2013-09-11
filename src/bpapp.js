5e6326dd65b992e75661877cb4bedd9f55553c15
Jacob Salomonsen <jiekebo@gmail.com>
2013-09-11 16:43:52 +0200

    Added filtering of trains for temporary raphael graphics.

--- src/bpapp.js
+++ src/bpapp.js
@@ -4,6 +4,7 @@ canvas.canvas.width = window.innerWidth;
 canvas.canvas.height = window.innerHeight;*/
 
 var debugTrack = "B";
+var yqlurl = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20data.uri%20where%20url%20%3D%20%22http%3A%2F%2Fbyenspuls.dsb.dk%2Fbyens_puls%2FBPServlet%22&format=json&callback='
 
 var raphael;
 var tc = new TrackConverter();
@@ -15,13 +16,15 @@ $(document).ready(function () {
 });
 
 function main() {
-    $.get('http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20data.uri%20where%20url%20%3D%20%22http%3A%2F%2Fbyenspuls.dsb.dk%2Fbyens_puls%2FBPServlet%22&format=json&callback=',
-    function (data) {
-        var togdata = atob(data.query.results.url.split(',')[1]);
-        bp.parse(togdata);
-        tc.calculateTrainPercentages(bp.bp.getTogListe());
-        drawRaphaelTrack(bp.bp.getTogListe());
-    });
+    $.get(
+        yqlurl,
+        function (data) {
+            var togdata = atob(data.query.results.url.split(',')[1]);
+            bp.parse(togdata);
+            tc.calculateTrainPercentages(bp.bp.getTogListe());
+            drawRaphaelTrack(bp.bp.getTogListe());
+        }
+    );
     setTimeout(main, 5000);
 }
 
@@ -34,11 +37,13 @@ function drawRaphaelTrack(tog) {
     var len = p.getTotalLength();
 
     for(trainId in tog) {
-        var trainPosition = tog[trainId].percentage;
-        var point = p.getPointAtLength(trainPosition * len);
-        raphael.circle(point.x, point.y, 5).attr({
-            stroke: "none",
-            fill: "#fff"
-        });
+        if(tog[trainId].data.linie[0] == debugTrack) {
+            var trainPosition = tog[trainId].percentage;
+            var point = p.getPointAtLength(trainPosition * len);
+            raphael.circle(point.x, point.y, 5).attr({
+                stroke: "none",
+                fill: "#fff"
+            });
+        }        
     }
 }
