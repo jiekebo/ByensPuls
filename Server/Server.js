eda9404902c16a51d15cda912b178d809ece77ed
Jacob Salomonsen <jiekebo@gmail.com>
2014-11-16 17:25:13 +0100

    Made server restart whenever a new user visits the page.

--- Server/Server.js
+++ Server/Server.js
@@ -1,9 +1,17 @@
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
 var count = 0;
+var id = 0;
 var file = './logs/bp';
 
 var server = http.createServer(function (req, res) {
+	var url_parts = url.parse(req.url, true);
+	if (url_parts.query.id != id) {
+                console.log("client with id " + url_parts.query.id + " connected.");
+		id = url_parts.query.id;
+		count = 0;
+	}
 	res.writeHead(200, {
 		'Content-Type': 'application/json',
 		'Access-Control-Allow-Origin': 'http://localhost:8000',
@@ -15,16 +23,6 @@ var server = http.createServer(function (req, res) {
 	res.end(fileBuffer);
 	console.log("loaded file " + file + count);
 	count++;
-
-	req.on('close', function() {
-		count = 0;
-	})
 });
 
-server.listen(8001, function() {
-	console.log("server bound!");
-});
-server.on('connection', function() {
-	console.log("Client connected");
-	count = 0;
-});
+server.listen(8001, function() {});
--- src/Controller.js
+++ src/Controller.js
@@ -16,7 +16,7 @@ var yqlurl = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20dat
 var yqlurl1 = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20data.uri%20where%20url%20%3D%20%22http%3A%2F%2Fbyenspuls.dsb.dk%2Fbyens_puls%2FBPServlet%3FTID%3D';
 var yqlurl2 = '%26ID%3D';
 var yqlurl3 = '%22&format=json&callback=';
-var testUrl = 'http://localhost:8001';
+var testUrl = 'http://188.226.167.211/bpserver?id=';
 
 var id = new Date().getTime();
 var tid = 0;
@@ -63,7 +63,7 @@ $(document).ready(function() {
 function main() {
     $.get(
         //yqlurl1 + tid + yqlurl2 + id + yqlurl3,
-        testUrl,
+        testUrl + id,
         function(data) {
             var togdata = atob(data.query.results.url.split(',')[1]);
             byenspuls.parse(togdata);
